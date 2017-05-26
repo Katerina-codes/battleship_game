b7548ec09b0fa32b89e159fa53936ce110b7139f
Katerina <katerinaldg@gmail.com>
2017-05-26 10:37:51 +0100

    Draws a grid thats 2 by 2

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,35 +1,8 @@
 class Grid
 
   def draw_grid(number)
-    board = Array.new(number, ".")
-    board = board.join
-    number = number.to_s
-    [number, board]
+    board = Array.new(number) { Array.new(number, ".")}
+    
   end
-  #   board = Array.new(number, ".")
-  #
-  #   x = 0
-  #   grid_number = 1
-  #
-  #   until x == number
-  #     print "\t"
-  #     if grid_number <= 9
-  #       print " "
-  #       print grid_number
-  #       print "  "
-  #     else
-  #       print grid_number
-  #       print "  "
-  #     end
-  #   print board
-  #   puts "\n"
-  #   x += 1
-  #   grid_number += 1
-  # end
-
-  # board
-  # grid_number
-
-
 
 end
