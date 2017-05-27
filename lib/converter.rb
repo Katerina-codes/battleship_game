af439b99910a644caf2189f39d01b22a14eddf56
Katerina <katerinaldg@gmail.com>
2017-05-27 22:49:57 +0100

    Moved converting method from Grid to Converter class

---
+++ lib/converter.rb
@@ -0,0 +1,19 @@
+class Converter
+
+  def numbers_to_letters(number)
+    letter_lookup = {
+      1 => " A ",
+      2 => " A B ",
+      3 => " A B C ",
+      4 => " A B C D ",
+      5 => " A B C D E ",
+      6 => " A B C D E F ",
+      7 => " A B C D E F G ",
+      8 => " A B C D E F G H ",
+      9 => " A B C D E F G H I ",
+      10 => " A B C D E F G H I J "
+    }
+    letter_lookup[number]
+  end
+
+end
---
+++ spec/converter_spec.rb
@@ -0,0 +1,17 @@
+require 'converter'
+
+describe Converter do
+
+    it "draws letter A for grid of 1" do
+      expect(subject.numbers_to_letters(1)).to eq(" A ")
+    end
+
+    it "draws letters A, B for grid of 2" do
+      expect(subject.numbers_to_letters(2)).to eq(" A B ")
+    end
+
+    it "draws letters A - J for grid of 10" do
+      expect(subject.numbers_to_letters(10)).to eq(" A B C D E F G H I J ")
+    end
+
+end
