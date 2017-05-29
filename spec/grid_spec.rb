8a1d3963f798e4c81dd8643e878a0a2abf4f42e3
Katerina <katerinaldg@gmail.com>
2017-05-29 13:57:47 +0100

    Tests removed from grid and put in converter spec

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -13,19 +13,6 @@ describe Grid do
 [".", "."]])
   end
 
-  it "draws letter A for grid of 1" do
-    expect(subject.get_letters(1)).to eq(" A ")
-  end
-
-  it "draws letters A, B for grid of 2" do
-    expect(subject.get_letters(2)).to eq(" A B ")
-  end
-
-  it "draws letters A - J for grid of 10" do
-    expect(subject.get_letters(10)).to eq(" A B C D E F G H I J ")
-  end
-
-
   it "marks first position on the grid" do
     grid = subject.draw_grid(2)
     x_coordinate = 0
