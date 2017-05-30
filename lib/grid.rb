40fa96b34ae6440808a39f2b86417b755254140d
Katerina <katerinaldg@gmail.com>
2017-05-30 11:55:27 +0100

    Removed comments and dead code

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,16 +1,8 @@
 class Grid
 
-  #   A B C
-  # 1 . . .
-  # 2 . . .
-  # 3 . . .
-
-  # C3 -> grid[2][2]
-
   def draw_grid(number)
     Array.new(number) { Array.new(number, ".")}
   end
-
 
   def mark_position(grid, x_coordinate, y_coordinate, mark)
     grid[x_coordinate][y_coordinate] = mark
--- lib/player.rb
+++ lib/player.rb
@@ -4,18 +4,6 @@ class Player
     @input = input
   end
 
-  def make_move(move, grid)
-
-    move = grid[0][0]
-    p move = "x"
-    p grid
-    # if grid == [["."]]
-    #   [["x"]]
-    # else
-    #   [["."], ["x"]]
-    # end
-  end
-
   def get_number_coordinate
     @input.gets.chomp
   end
