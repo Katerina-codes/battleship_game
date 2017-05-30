f2521213ab795302670d4f3a19ca7059b962140c
Katerina <katerinaldg@gmail.com>
2017-05-30 11:34:14 +0100

    Aks the player for a letter

--- lib/display.rb
+++ lib/display.rb
@@ -23,11 +23,15 @@ class Display
     final_grid = formatted_grid.each_with_index.map do |row, index|
       @output.print (index + 1).to_s + " ", row
     end
+  end  
 
     def ask_for_number
       @output.puts "Time to make a move. Please enter a number from 1 - 10"
     end
 
-  end
+    def ask_for_letter
+      @output.puts "Please enter a letter from A - J"
+    end
+
 
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -15,4 +15,12 @@ describe Display do
     display.ask_for_number
     expect(output.string).to eq("Time to make a move. Please enter a number from 1 - 10\n")
   end
+
+  it "Asks the player for a letter" do
+    output = StringIO.new
+    display = Display.new(output)
+    display.ask_for_letter
+    expect(output.string).to eq("Please enter a letter from A - J\n")
+  end
+
 end
