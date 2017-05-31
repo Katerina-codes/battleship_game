f2dbf35cd313c4c21237369708a6eb8caad8d378
Katerina <katerinaldg@gmail.com>
2017-05-31 15:52:51 +0100

    Returns true if number = 1

--- lib/player.rb
+++ lib/player.rb
@@ -8,6 +8,10 @@ class Player
     @input.gets.chomp
   end
 
+  def is_number_valid?(number)
+    true
+  end
+
   def get_letter_coordinate
     @input.gets.chomp.downcase
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,6 +8,11 @@ describe Player do
     expect(player.get_number_coordinate).to eq("1")
   end
 
+  it "only allows numbers from 1-10" do
+    player = Player.new
+    expect(player.is_number_valid?("1")).to eq(true)
+  end
+  
   it "gets a letter from a player" do
     input = StringIO.new("A")
     player = Player.new(input)
