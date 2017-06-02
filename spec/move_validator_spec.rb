ccf8f36e028c6eee8eefe124b9ae1c4f27f13dae
Katerina <katerinaldg@gmail.com>
2017-06-02 11:29:33 +0100

    Refactored tests to make them shorter

--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -2,29 +2,22 @@ require 'move_validator'
 
 describe MoveValidator do
 
-  it "only allows numbers from 1-10" do
-    move_validator = MoveValidator.new
+  let(:move_validator) {MoveValidator.new}
 
+  it "only allows numbers from 1-10" do
     expect(move_validator.is_number_valid?(1)).to eq(true)
   end
 
   it "only allows numbers from 1-10" do
-    move_validator = MoveValidator.new
-
     expect(move_validator.is_number_valid?(0)).to eq(false)
   end
 
   it "Returns true if letter is from a - j" do
-    move_validator = MoveValidator.new
-
     expect(move_validator.is_letter_valid?("a")).to eq(true)
   end
 
   it "Returns false if letter is k" do
-    move_validator = MoveValidator.new
-
     expect(move_validator.is_letter_valid?("k")).to eq(false)
   end
-
 
 end
