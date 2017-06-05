4689e045ffd653ae0f6ff22353c463b22392d8ca
Katerina <katerinaldg@gmail.com>
2017-06-05 16:08:08 +0100

    Refactored tests

--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -2,34 +2,28 @@ require 'move_validator'
 
 describe MoveValidator do
 
-  let(:move_validator) {MoveValidator.new}
-
   it "only allows numbers from 1-10" do
     display = Display.new
     player = Player.new
-    move_validator = MoveValidator.new
-    expect(move_validator.is_number_valid?(1)).to eq(true)
+    expect(subject.is_number_valid?(1)).to eq(true)
   end
 
   it "only allows numbers from 1-10" do
     display = Display.new
     player = Player.new
-    move_validator = MoveValidator.new
-    expect(move_validator.is_number_valid?(0)).to eq(false)
+    expect(subject.is_number_valid?(0)).to eq(false)
   end
 
   it "Returns true if letter is from a - j" do
     display = Display.new
     player = Player.new
-    move_validator = MoveValidator.new
-    expect(move_validator.is_letter_valid?("a")).to eq(true)
+    expect(subject.is_letter_valid?("a")).to eq(true)
   end
 
   it "Returns false if letter is k" do
     display = Display.new
     player = Player.new
-    move_validator = MoveValidator.new
-    expect(move_validator.is_letter_valid?("k")).to eq(false)
+    expect(subject.is_letter_valid?("k")).to eq(false)
   end
 
 end
