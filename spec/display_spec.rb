afbd677ae171bb75c927d7237083409cad5e5737
Katerina <katerinaldg@gmail.com>
2017-06-05 18:29:24 +0100

    Refactored Display class tests

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,31 +1,25 @@
 require 'display'
 
 describe Display do
+  let(:output) { StringIO.new }
+  let(:display) { Display.new(output) }
 
   it "displays grid of 1 by 1 with letter A" do
-    output = StringIO.new
-    display = Display.new(output)
     display.display_grid(1)
     expect(output.string).to include(" A ", ".")
   end
 
   it "Asks the player for a number coordinate" do
-    output = StringIO.new
-    display = Display.new(output)
     display.ask_for_number
     expect(output.string).to eq("Time to make a move. Please enter a number from 1 - 10\n")
   end
 
   it "Asks the player for a letter" do
-    output = StringIO.new
-    display = Display.new(output)
     display.ask_for_letter
     expect(output.string).to eq("Please enter a letter from A - J\n")
   end
 
   it "displays the newest version of the grid" do
-    output = StringIO.new
-    display = Display.new(output)
     display.display_lastest_grid([["X"]], 1)
     expect(output.string).to include("A", "X")
   end
