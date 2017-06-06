e9abfb3fccbe241ccda902f274eb2f4c4937b626
Katerina <katerinaldg@gmail.com>
2017-06-06 09:08:46 +0100

    Removed dead code

--- lib/move_validator.rb
+++ lib/move_validator.rb
@@ -3,11 +3,6 @@ require_relative 'display'
 
 class MoveValidator
 
-  # def initialize(display, player)
-  #   @display = display
-  #   @player = player
-  # end
-
   def is_number_valid?(number)
     number.to_i >= 1 && number.to_i <= 10
   end
