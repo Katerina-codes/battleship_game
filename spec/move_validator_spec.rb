621ffbc6dd83a97e4ec9d13bf577b0497af197db
Katerina <katerinaldg@gmail.com>
2017-06-06 15:52:16 +0100

    Used let to remove explicit instance creation in each test

--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -1,24 +1,21 @@
 require 'move_validator'
 
 describe MoveValidator do
+  let(:display) { Display.new }
 
   it "only allows numbers from 1-10" do
-    display = Display.new
     expect(subject.is_number_valid?(1)).to eq(true)
   end
 
   it "only allows numbers from 1-10" do
-    display = Display.new
     expect(subject.is_number_valid?(0)).to eq(false)
   end
 
   it "Returns true if letter is from a - j" do
-    display = Display.new
     expect(subject.is_letter_valid?("a")).to eq(true)
   end
 
   it "Returns false if letter is k" do
-    display = Display.new
     expect(subject.is_letter_valid?("k")).to eq(false)
   end
 
