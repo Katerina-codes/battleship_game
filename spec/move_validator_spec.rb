a3e4de048b65b9777a5c7220c6e53f1c56ee6579
Katerina <katerinaldg@gmail.com>
2017-06-06 15:49:38 +0100

    Removed unused creation of player instance

--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -4,25 +4,21 @@ describe MoveValidator do
 
   it "only allows numbers from 1-10" do
     display = Display.new
-    player = Player.new
     expect(subject.is_number_valid?(1)).to eq(true)
   end
 
   it "only allows numbers from 1-10" do
     display = Display.new
-    player = Player.new
     expect(subject.is_number_valid?(0)).to eq(false)
   end
 
   it "Returns true if letter is from a - j" do
     display = Display.new
-    player = Player.new
     expect(subject.is_letter_valid?("a")).to eq(true)
   end
 
   it "Returns false if letter is k" do
     display = Display.new
-    player = Player.new
     expect(subject.is_letter_valid?("k")).to eq(false)
   end
 
