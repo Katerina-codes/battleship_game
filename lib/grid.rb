9e37f4b7c43ae24ba5c4017d5fc4e9ed37b1b551
Katerina <katerinaldg@gmail.com>
2017-06-06 09:10:30 +0100

    Added a space

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,7 +1,7 @@
 class Grid
 
   def draw_grid(number)
-    Array.new(number) { Array.new(number, ".")}
+    Array.new(number) { Array.new(number, ".") }
   end
 
   def mark_position(grid, x_coordinate, y_coordinate, mark)
