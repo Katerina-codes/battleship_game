6b4befae30892a9b2bd8667de5611a7b0d1646c3
Katerina <katerinaldg@gmail.com>
2017-06-06 16:14:29 +0100

    Does not allow more than one character to be entered. Corrected test input in same file

--- lib/move_validator.rb
+++ lib/move_validator.rb
@@ -4,7 +4,8 @@ require_relative 'display'
 class MoveValidator
 
   def is_number_valid?(number)
-    number.to_i >= 1 && number.to_i <= 10
+    number
+    number.to_i >= 1 && number.to_i <= 10 && number.length <= 1
   end
 
   def is_letter_valid?(letter)
--- spec/move_validator_spec.rb
+++ spec/move_validator_spec.rb
@@ -3,11 +3,11 @@ require 'move_validator'
 describe MoveValidator do
 
   it "only allows numbers from 1-10" do
-    expect(subject.is_number_valid?(1)).to eq(true)
+    expect(subject.is_number_valid?("1")).to eq(true)
   end
 
   it "only allows numbers from 1-10" do
-    expect(subject.is_number_valid?(0)).to eq(false)
+    expect(subject.is_number_valid?("0")).to eq(false)
   end
 
   it "Returns true if letter is from a - j" do
@@ -17,5 +17,9 @@ describe MoveValidator do
   it "Returns false if letter is k" do
     expect(subject.is_letter_valid?("k")).to eq(false)
   end
+
+it "does not allow more than one character to be entered" do
+  expect(subject.is_number_valid?("4d")).to eq(false)
+end
 
 end
