a310fbc1f6e82a0fe333c17ceb52ef43c7d654d7
Katerina <katerinaldg@gmail.com>
2017-06-13 21:23:18 +0100

    Corrected number validator function

--- lib/move_validator.rb
+++ lib/move_validator.rb
@@ -4,7 +4,7 @@ require_relative 'display'
 class MoveValidator
 
   def is_number_valid?(number)
-    number.to_i >= 1 && number.to_i <= 10 && number.length <= 1
+    number.to_i >= 1 && number.to_i <= 10 && number.length <= 1 || number == "10"
   end
 
   def is_letter_valid?(letter)
