cf3d6920e45d4581cb8582e87917a3fd35056b44
Katerina <katerinaldg@gmail.com>
2017-06-14 14:24:07 +0100

    Updated Computer play_move method so it can return numerous coordinates

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,7 +1,18 @@
 class ComputerPlayer
 
-  def play_move
-    [0, 0]
+  def initialize(moves = (1..10).to_a)
+    @moves = moves
+  end
+
+  def play_move(number_of_ship_coordinates)
+    ship_coordinates = []
+
+    until number_of_ship_coordinates == 0
+      move = [@moves.sample, @moves.sample]
+      number_of_ship_coordinates -= 1
+      ship_coordinates.push(move)
+    end
+    ship_coordinates
   end
 
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -2,8 +2,19 @@ require 'computer_player'
 
 describe ComputerPlayer do
 
-  it "returns a move when it is passed a move" do
-    computer = ComputerPlayer.new
-    expect(computer.play_move).to eq([0, 0]])
+  it "returns [0, 0] if passed [0, 0]" do
+    computer = ComputerPlayer.new([0, 0])
+    expect(computer.play_move(1)).to eq([[0, 0]])
   end
+  #
+  # it "returns a random move if no move is passed" do
+  #   computer = ComputerPlayer.new
+  #   expect(computer.play_move(1)).to satisfy { |value_returned| value_returned == [1, 2] }
+  # end
+  #
+  # it "returns 2 coordinates if 2 are needed" do
+  #   computer = ComputerPlayer.new
+  #   expect(computer.play_move(2)).to eq([[0, 0], [0, 1]])
+  # end
+
 end
