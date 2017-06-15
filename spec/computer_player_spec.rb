19bc0883800fb65df59533f93768b1f63e8cab1a
Katerina <katerinaldg@gmail.com>
2017-06-15 09:26:41 +0100

    Returns an array of 2 arrays if 2 coordinates are needed

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -9,18 +9,19 @@ describe ComputerPlayer do
 
   it "returns a random move if no move is passed" do
     computer = ComputerPlayer.new
-    p array_returned = computer.play_move(2)
+    array_returned = computer.play_move(2)
     valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    puts x = array_returned[0][0]
-    puts y = array_returned[0][1]
+    x = array_returned[0][0]
+    y = array_returned[0][1]
 
     expect(valid_numbers.include?(x)).to eq(true)
     expect(valid_numbers.include?(y)).to eq(true)
   end
 
-  # it "returns 2 coordinates if 2 are needed" do
-  #   computer = ComputerPlayer.new
-  #   expect(computer.play_move(2)).to eq([[0, 0], [0, 1]])
-  # end
+  it "returns array of 2 arrays if 2 coordinates are needed" do
+    computer = ComputerPlayer.new
+    array_returned = computer.play_move(3)
+    expect(array_returned.length).to eq(3)
+  end
 
 end
