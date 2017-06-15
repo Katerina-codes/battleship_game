62475a44c865074fbfeb2b3a7a445fdecef60302
Katerina <katerinaldg@gmail.com>
2017-06-15 15:30:13 +0100

    Refactored method

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,12 +3,12 @@ class ComputerPlayer
   def play_move(number_of_ship_coordinates, grid_size)
     ship_coordinates = []
 
-    until number_of_ship_coordinates == 0
+    number_of_ship_coordinates.times do |move|
       move = [(0..grid_size - 1).to_a.sample, (0..grid_size - 1).to_a.sample]
-      number_of_ship_coordinates -= 1
-      ship_coordinates.push(move)
+      ship_coordinates = ship_coordinates.push(move)
     end
-    ship_coordinates
+  ship_coordinates
   end
+
 
 end
