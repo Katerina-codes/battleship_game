5ff11ce8ec9593e989d6125e38deb0ba74ed5ae2
Katerina <katerinaldg@gmail.com>
2017-06-15 09:17:03 +0100

    Returns a random move if no move is passed

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -6,12 +6,18 @@ describe ComputerPlayer do
     computer = ComputerPlayer.new([0, 0])
     expect(computer.play_move(1)).to eq([[0, 0]])
   end
-  #
-  # it "returns a random move if no move is passed" do
-  #   computer = ComputerPlayer.new
-  #   expect(computer.play_move(1)).to satisfy { |value_returned| value_returned == [1, 2] }
-  # end
-  #
+
+  it "returns a random move if no move is passed" do
+    computer = ComputerPlayer.new
+    p array_returned = computer.play_move(2)
+    valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    puts x = array_returned[0][0]
+    puts y = array_returned[0][1]
+
+    expect(valid_numbers.include?(x)).to eq(true)
+    expect(valid_numbers.include?(y)).to eq(true)
+  end
+
   # it "returns 2 coordinates if 2 are needed" do
   #   computer = ComputerPlayer.new
   #   expect(computer.play_move(2)).to eq([[0, 0], [0, 1]])
