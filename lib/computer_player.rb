d76071b7f8fda19e9d5d3bd9ac78b3e3a4b10c46
Katerina <katerinaldg@gmail.com>
2017-06-15 14:20:24 +0100

    Removed initialize method and default value for ComputerPlayer. Adapted play_move so ComputerPlayer's moves are valid for grid size.

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,14 +1,10 @@
 class ComputerPlayer
 
-  def initialize(moves = (1..10).to_a)
-    @moves = moves
-  end
-
-  def play_move(number_of_ship_coordinates)
+  def play_move(number_of_ship_coordinates, grid_size)
     ship_coordinates = []
 
     until number_of_ship_coordinates == 0
-      move = [@moves.sample, @moves.sample]
+      move = [(0..grid_size - 1).to_a.sample, (0..grid_size - 1).to_a.sample]
       number_of_ship_coordinates -= 1
       ship_coordinates.push(move)
     end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -2,26 +2,29 @@ require 'computer_player'
 
 describe ComputerPlayer do
 
-  it "returns [0, 0] if passed [0, 0]" do
-    computer = ComputerPlayer.new([0, 0])
-    expect(computer.play_move(1)).to eq([[0, 0]])
-  end
+  # it "returns [0, 0] if passed [0, 0]" do
+  #   grid_size = 1
+  #   computer = ComputerPlayer.new([0, 0])
+  #   expect(computer.play_move(1, grid_size)).to eq([[0, 0]])
+  # end
 
   it "returns a random move if no move is passed" do
+    grid_size = 2
     computer = ComputerPlayer.new
-    array_returned = computer.play_move(2)
-    valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    x = array_returned[0][0]
-    y = array_returned[0][1]
+    ship_coordinates = computer.play_move(2, grid_size)
+    valid_numbers = (0..grid_size).to_a
+    x_coordinate = ship_coordinates[0][0]
+    y_coordinate = ship_coordinates[0][1]
 
-    expect(valid_numbers.include?(x)).to eq(true)
-    expect(valid_numbers.include?(y)).to eq(true)
+    expect(valid_numbers.include?(x_coordinate)).to eq(true)
+    expect(valid_numbers.include?(y_coordinate)).to eq(true)
   end
 
   it "returns array of 2 arrays if 2 coordinates are needed" do
+    grid_size = 2
     computer = ComputerPlayer.new
-    array_returned = computer.play_move(3)
-    expect(array_returned.length).to eq(3)
+    ship_coordinates = computer.play_move(3, grid_size)
+    expect(ship_coordinates.length).to eq(3)
   end
 
 end
